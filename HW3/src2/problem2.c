8be266dd723cfa091c48eec7ef2891bcab991a9f
Dan <danny321974345@gmail.com>
2018-05-01 13:06:16 +0800

    update

--- HW3/src2/problem2.c
+++ HW3/src2/problem2.c
@@ -1,8 +1,27 @@
+#include "defs.h"
+#include "file_utils.h"
+#include "boundary.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
+void laws_method(unsigned char (*out)[COL], unsigned char (*in)[COL]);
+
 int main(void)
 {
+    unsigned char data[ROW][COL];
+    unsigned char K[ROW][COL];
+    unsigned char L[ROW][COL];
+
+    open_and_read(data, "sample2.raw");
+
+    open_and_write(K, "K.raw");
+
+    open_and_write(L, "L.raw");
+
     return 0;
 }
 
+void laws_method(unsigned char (*out)[COL], unsigned char (*in)[COL])
+{
+}
